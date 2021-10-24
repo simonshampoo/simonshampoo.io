338bd328a60b16a274081867b71b1a43d5bc255d
Simon Shamoon <simonshm5@gmail.com>
2021-10-24 01:44:31 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,139 @@
-import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
+import * as React from "react";
+import styled from "@emotion/styled";
+import { Ethereum } from "@emotion-icons/simple-icons/Ethereum";
+import { Linkedin } from "@emotion-icons/bootstrap/Linkedin";
+import { Github } from "@emotion-icons/bootstrap/Github";
+import { DocumentBulletList } from "@emotion-icons/fluentui-system-filled/DocumentBulletList";
+import { Ruby } from "@emotion-icons/simple-icons/ruby";
+import { Typescript } from "@emotion-icons/simple-icons/TypeScript";
+import { Node } from "@emotion-icons/fa-brands/Node";
+import { Graphql } from "@emotion-icons/simple-icons/Graphql";
+import { ReactLogo } from "@emotion-icons/boxicons-logos/ReactLogo";
 // markup
+
+const Span = ({ color, children }) => {
+  return <span style={{ color: color }}>{children}</span>;
+};
+
+
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+      <div>
+        <h1
+          style={{
+            fontFamily: "TiemposHeadline",
+            color: "#111d33",
+            fontSize: 80,
+            marginBottom: 0,
+          }}
+        >
+          Hi, I'm Simon. 👋
+        </h1>
+        <a
+          href="https://www.linkedin.com/in/simon-shamoon"
+          style={{ marginLeft: "35%", color: "black", textDecoration: "none" }}
+        >
+          <Linkedin size="36" />
+        </a>
+        &nbsp;
+        <a
+          href="https://www.github.com/simonshm5"
+          style={{ color: "black", textDecoration: "none" }}
+        >
+          <Github size="36" />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1H2Olqh4MTa2akqiLmDUx7amh_ycyMk4y/view?usp=sharing"
+          style={{ color: "black", textDecoration: "none" }}
+        >
+          <DocumentBulletList size="36" />
+        </a>
+        <div style={{ fontSize: 20, width: 700, marginTop: 10 }}>
+          <code>
+            I am a junior at ASU studying computer science.
+            <h2 style={{ fontFamily: "TiemposHeadline", fontWeight: 900 }}>
+              My work experience:
+            </h2>
+            <ul>
+              <li>
+                Incoming SWE Intern @{" "}
+                <Span color="#003366" children={"Capital "} />
+                <Span color="#a12830" children={"One"} /> (Summer 2022)
+              </li>
+              <li>
+                SWE Intern @ <Span color="#343a40" children="Campus" />
+                <Span color="#007bff" children="Logic" /> (Oct 2021 - Present)
+              </li>
+              <li>Trader Intern @ Edward Jones (Jun 2021 - Aug 2021)</li>
+            </ul>
+            <h2 style={{ fontFamily: "TiemposHeadline", fontWeight: 900 }}>
+              Things I enjoy:
+            </h2>
+            <ul>
+              <li>
+                programming{" "}
+                <Span
+                  color="gray"
+                  children="// TODO: remove this, we already know -_-"
+                />
+              </li>
+              <li>blockchain technology & DeFi</li>
+              <li>algorithms and discrete math</li>
+              <li>Dota 2</li>
+              <li>sparkling water & coffee</li>
+              <li>dogs c･ﻌ･ɔ</li>
+            </ul>
+            <h2 style={{ fontFamily: "TiemposHeadline", fontWeight: 900 }}>
+              Things I'm currently learning:
+            </h2>
+            <ul>
+              <li>
+                <Span color="red" children={<Ruby size="24" />} />,{" "}
+                <Span color="#007ACC" children={<Typescript size="24" />} />,{" "}
+                <Span color="#68A063" children={<Node size="36" />} />,{" "}
+                <Span color="#e535ab" children={<Graphql size="24" />} />,{" "}
+                <Span color="#61DBFB" children={<ReactLogo size="28" />} />
+              </li>
+              <li>
+                blockchain dev & the ethereum ecosystem
+                <Ethereum size="24" />
+              </li>
+              <li>UI/UX design</li>
+              <li>generative art, creative coding, & NFTs</li>
+              <li>
+                full stack dev (This site's built with
+                <span style={{ color: "rebeccapurple" }}> Gatsby</span>
+                !)
+              </li>
+            </ul>
+          </code>
+          {/* <button
+            style={{
+              backgroundColor: "#416dea",
+              marginLeft: "30%",
+              fontSize: 20,
+              color: "white",
+              border: "none",
+              textAlign: "center",
+              height: 50,
+              width: 200,
+              borderRadius: 36,
+            }}
           >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+            <code>Get in touch!</code>
+          </button> */}
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;

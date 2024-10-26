9dfb7dcc9340604fbf89d68e26fb834bea7dfb14
simon <71435388+simonshampoo@users.noreply.github.com>
2024-10-26 14:13:41 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,51 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 import "98.css";
 import Draggable from "react-draggable"; // The default
-
-const IndexPage = () => {
-    const [visible, setVisible] = useState(true);
-
-    useEffect(() => {
-        document.body.style.backgroundImage =
-            "url('https://forums.rpgmakerweb.com/data/attachments/90/90682-271a42b91433b94db629d7a72f61233b.jpg')";
-          document.body.style.backgroundSize = "cover";
-    }, []);
-
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <title>simonshampoo</title>
-            {visible && (
-                <Draggable cancel=".link">
-                    <div className="window">
-                        <div className="title-bar">
-                            <div className="title-bar-text">
-                                simonshampoo-x86.exe
-                            </div>
-                            <div className="title-bar-controls">
-                                <button
-                                    aria-label="Close"
-                                    onClick={() => setVisible(false)}
-                                />
-                            </div>
-                        </div>
-                        <div
-                            className="window-body"
-                            style={{
-                                fontSize: "large",
-                                textAlign: "center",
-                                margin: 30,
-                            }}
-                        >
-                            <b>
-                                <a
-                                    className="link"
-                                    href="https://www.github.com/simonshampoo"
+ampoo"
                                 >
                                     github,
                                 </a>
